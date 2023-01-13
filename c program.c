8a92d3045ad9a7d1436b55c40d443a679db68187
pratham40 <122166099+pratham40@users.noreply.github.com>
2023-01-13 01:15:07 -0800

    Add files via upload

---
+++ c program.c
@@ -0,0 +1,341 @@
+/*include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");    
+return 0;  
+}
+
+
+include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,number;    
+ printf("Enter the number of elements:");    
+ scanf("%d",&number);    
+ printf("\n%d %d",n1,n2);//printing 0 and 1    
+ for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }
+
+
+ /Create a C Program to find the perfect number using while loop/  
+#include<stdio.h>  
+#include<conio.h>  
+void main()  
+{  
+int i = 1, num, Sum = 0;  
+printf(" Enter any number to check Perfect Number \n");  
+scanf("%d", &num);  
+  
+while(i < num )  
+                     {  
+                               if(num % i == 0)  
+                               Sum = Sum + i;  
+                               i++;  
+                     }  
+           if(Sum == num)  
+                  printf("\n %d is Perfect Number", num);  
+           else  
+           printf("\n %d is not a Perfect Number", num);  
+getch();  
+}
+
+
+#include <stdio.h>
+
+int main() {
+
+    int n1, n2, max;
+
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    // maximum number between n1 and n2 is stored in max
+    max = (n1 > n2) ? n1 : n2;
+
+    while (1) {
+        if ((max % n1 == 0) && (max % n2 == 0)) {
+            printf("The LCM of %d and %d is %d.", n1, n2, max);
+            break;
+        }
+        ++max;
+    }
+    return 0;
+}
+
+include <stdio.h>
+int main()
+{
+    int n1, n2, i, gcd;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)
+    {
+        // Checks if i is factor of both integers
+        if(n1%i==0 && n2%i==0)
+            gcd = i;
+    }
+
+    printf("G.C.D of %d and %d is %d", n1, n2, gcd);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", j);
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+int main() {
+   int i, j;
+   char input, alphabet = 'A';
+   printf("Enter an uppercase character you want to print in the last row: ");
+   scanf("%c", &input);
+   for (i = 1; i <= (input - 'A' + 1); ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%c ", alphabet);
+      }
+      ++alphabet;
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", j);
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+#include <stdio.h>
+int main() {
+   int i, space, rows, k = 0;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i, k = 0) {
+      for (space = 1; space <= rows - i; ++space) {
+         printf("  ");
+      }
+      while (k != 2 * i - 1) {
+         printf("* ");
+         ++k;
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+include <stdio.h>
+int main() {
+   int i, space, rows, k = 0, count = 0, count1 = 0;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (space = 1; space <= rows - i; ++space) {
+         printf("  ");
+         ++count;
+      }
+      while (k != 2 * i - 1) {
+         if (count <= rows - 1) {
+            printf("%d ", i + k);
+            ++count;
+         } else {
+            ++count1;
+            printf("%d ", (i + k - 2 * count1));
+         }
+         ++k;
+      }
+      count1 = count = k = 0;
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+int main() {
+   int rows, i, j, space;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (space = 0; space < rows - i; ++space)
+         printf("  ");
+      for (j = i; j <= 2 * i - 1; ++j)
+         printf("* ");
+      for (j = 0; j < i - 1; ++j)
+         printf("* ");
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+ 
+int main()
+{
+    int number, rem, sum = 0, i;
+ 
+    printf("Enter a Number: ");
+    scanf("%d", &number);
+    for (i = 1; i <= (number - 1); i++)
+    {
+        rem = number % i;
+	if (rem == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    if (sum == number)
+        printf("%d is perfect number", number);
+    else
+        printf("%d is not a perfect number", number);
+    return 0;
+}
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", j);
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+int main() {
+   int i, j;
+   char input, alphabet = 'A';
+   printf("Enter an uppercase character you want to print in the last row: ");
+   scanf("%c", &input);
+   for (i = 1; i <= (input - 'A' + 1); ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%c ", alphabet);
+      }
+      ++alphabet;
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+*/
